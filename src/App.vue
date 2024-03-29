3ae6aa3cb42e51352695400c3d6db7e8e36e5ce8
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-03-30 01:22:12 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+export default {
+  data() {
+    return {
+      apiData: null
+    }
+  },
+
+  mounted() {
+    const apiUrl = 'https://api.example.com/data' // Replace with your API endpoint URL
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        this.apiData = response.data
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error)
+      })
+  }
+}
+</script>
+
+<template>
+  <header></header>
+
+  <main></main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
